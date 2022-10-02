7d1017ddfb0e086ff1e0da6a0d5afa4527358444
chrisalecafe <chris.fue9768@gmail.com>
2022-10-02 01:40:27 +0000

    Feat: hello world made in gitpod

---
+++ helloworld/src/App.js
@@ -0,0 +1,15 @@
+;
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/telus-logo-2020.png" className="App-logo" alt="logo" />
+        <h3>Hellow world!</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
